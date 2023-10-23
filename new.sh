b79f14ce755cae484bdee526ef233afb184d0142
Jun Gu <druidream@gmail.com>
2023-10-23 11:57:34 +0800

    use problem id as solve func name

--- new.sh
+++ new.sh
@@ -3,7 +3,9 @@ read filename
 mkdir ${filename}
 cd ${filename}
 # touch main.cpp
+sed -i '' 's/solve/'${filename}'/g' ~/Developer/competitive-programming/cpp/cpp/main.cpp
 cp ~/Developer/competitive-programming/cpp/cpp/main.cpp .
+sed -i '' 's/'${filename}'/solve/g' ~/Developer/competitive-programming/cpp/cpp/main.cpp
 touch input.txt
 touch output.txt
 # cd ..
